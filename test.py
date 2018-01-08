2ac95f79b6fdb5afa93fc7502a98e02a0ab482db
janrems <jan1.rems@gmail.com>
2018-01-08 21:10:09 +0100

    sd

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
 import re
 def vsota(niz):
-    rx = re.compile(r'a(.*?),b')
-    return re.findall(rx,niz)
+    rx = re.compile('.*?<table class="wikitable sortable">'
+                    '(.*)'
+                    '</table>.*?')
+    x = re.findall(rx,niz)
+    return x
 
-def vsota2(a,b):
-    return a+b
+def vsota1(niz):
+    rx = re.compile('a'
+                    '(.*)'
+                    'b')
+    x = re.findall(rx,niz)
+    return x
 
-def vp(vsota2,a,b):
-    return vsota2(a,b)*b
+def vsota2(niz):
+    rx = re.compile('a'
+                    '(.*?)'
+                    '"')
+    x = re.findall(rx,niz)
+    return x
