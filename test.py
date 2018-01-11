794af947c596350c059fe66dff4f92c5981887fe
janrems <jan1.rems@gmail.com>
2018-01-11 01:58:20 +0100

    pobrane strani

--- test.py
+++ test.py
@@ -8,14 +8,24 @@ def vsota(niz):
 
 def vsota1(niz):
     rx = re.compile('a'
-                    '(.*)'
+                    '(.*?)'
                     'b')
     x = re.findall(rx,niz)
     return x
 
 def vsota2(niz):
-    rx = re.compile('a'
-                    '(.*?)'
-                    '"')
+    rx = re.compile(r'<'
+                    r'(.*?)'
+                    r'\\')
     x = re.findall(rx,niz)
     return x
+x = [1,2,3,4]
+for i in range (0,len(x)):
+    print(x[i])
+
+
+str= 'sda,sd'
+l = str.split(',')
+print(x)
+for a in x:
+    a = 1
