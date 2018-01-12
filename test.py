59fe8ea87416d9b54210c51f38bc9ac2295c385c
janrems <jan1.rems@gmail.com>
2018-01-12 02:15:21 +0100

    sez. slovarjev

--- test.py
+++ test.py
@@ -29,3 +29,33 @@ l = str.split(',')
 print(x)
 for a in x:
     a = 1
+
+nz= re.search(r'(^[A-Za-z]$)|((.*)(,|\s)(\#(\d+))?)','Harry')
+k = re.search(r'.*','abc dsf,sd,')
+r'(.*)(,|\s)\#(\d+)'
+s = re.sub(r'(d)$','','abc dsf,sdl')
+
+re.compile('googletag\.pubads\(\)\.setTargeting\("shelf", '
+                               '(?P<zvrst>.*?)\).*?'
+                               '<span class="item" style="display:none"><span class="fn">'
+                               '(?P<naslov>.*?)'
+                               '( \(.*?(?P<del_serije>.*?)\))?'
+                               '<'
+                               '.*?ratingValue">'
+                               '(?P<ocena>.*?)'
+                               '<.*?'
+                               'ratingCount" content="'
+                               '(?P<st_ocen>.*?)'
+                               '".*?'
+                               'class="uitext darkGreyText">.*?'
+                               '(numberOfPages">?'
+                               '(?P<st_strani>\d*?)?'
+                               ' .*?<.*?'
+                               '<div class="row">'
+                               '.*?(?P<leto_izdaje>\d{4}).*?'
+                               '(<nobr class="greyText">.*?)?'
+                               '((?P<leto_izida>\d{4}).*?)?'
+                               '<.*?'
+                               '(<div class="infoBoxRowItem" itemprop=\'inLanguage\'>(?P<jezik_izdaje>.*?))?'
+                               '<.*?',
+                               re.DOTALL)
